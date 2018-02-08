d5743c067841770c5b09314eb373ee683e388e81
Mike Lange <Mike.Lange@jda.com>
2018-02-08 13:39:10 -0600

    First commit to GitHub

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <moca.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <oslib.h>
+
+
+void main(int argc, char *argv[])
+{
+    printf("Size: %d\n", sizeof(fd_set));
+
+    exit(0);
+}
